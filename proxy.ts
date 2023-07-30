e70eeb6f9cc8e6c29e0e63476e6a3fcd8477f9fd
Eduardo Airaudo <eduardo@optimmus.io>
2023-07-30 20:34:34 -0300

    feat: proxy

---
+++ proxy.ts
@@ -0,0 +1,2 @@
+const privateProxy = 'socks5://38.127.172.90:39080';
+export default privateProxy;
--- warframe.ts
+++ warframe.ts
@@ -1,13 +1,21 @@
-import axios from "axios";
+import axios, { AxiosInstance } from "axios";
+import { SocksProxyAgent } from "socks-proxy-agent";
 import { MongooseServer, Schema } from "./database";
 import { Item, OrdersWarframe, StatisticsWarframe, WarframeItemSingle, WarframeItems } from "./interface";
+import privateProxy from "./proxy";
 class Warframe {
     db: MongooseServer;
+    axios: AxiosInstance;
     constructor() {
         this.db = MongooseServer.getInstance('warframe-items', new Schema({
           id: { type: String, unique: true },
         },
-        { strict: false }))
+            { strict: false }))
+        const proxy = new SocksProxyAgent(privateProxy), ;
+        this.axios = axios.create({
+            httpAgent: proxy,
+            httpsAgent: proxy
+        });
     }
     async saveItem(id: string, item:any) {
         await this.db.getAnUpdateEntry({id}, item)
@@ -22,17 +30,17 @@ class Warframe {
         return await this.db.allEntries({});
     }
     async getWarframeItems() : Promise<WarframeItems> {
-        const res = await axios.get('https://api.warframe.market/v1/items').then(res => res.data);
+        const res = await this.axios.get('https://api.warframe.market/v1/items').then(res => res.data);
         return res;
     }
     async getSingleItemData(item: Item): Promise<WarframeItemSingle> {
         const url = `https://api.warframe.market/v1/items/${item.url_name}`;
-        const res = await axios.get(url).then(res => res.data);
+        const res = await this.axios.get(url).then(res => res.data);
         return res;
     }
     async getWarframeItemOrders(item: Item): Promise<{buy: number, sell:number, volume:number}> {
         const url = `https://api.warframe.market/v1/items/${item.url_name}/orders`;
-        const res = await axios.get(url).then(res => res.data);
+        const res = await this.axios.get(url).then(res => res.data);
         const itemSet = item.items_in_set[0];
         let max_rank = itemSet.mod_max_rank;
         console.log("Max Rank", max_rank);
@@ -41,7 +49,7 @@ class Warframe {
     }
     async getWarframeItemStatistics(item: Item, max_rank: number | undefined): Promise<{volume:number}> {
         const url = `https://api.warframe.market/v1/items/${item.url_name}/statistics`;
-        const res: StatisticsWarframe = await axios.get(url).then(res => res.data);
+        const res: StatisticsWarframe = await this.axios.get(url).then(res => res.data);
         const volume = res.payload.statistics_closed['48hours'].reduce((prev, curr) => (max_rank === undefined || curr.mod_rank === max_rank) ? prev + curr.volume: prev, 0);
         return {
             volume
