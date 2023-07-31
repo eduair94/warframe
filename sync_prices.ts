19b279c633813c35265a6f8872425e2bfd30c1a3
Eduardo Airaudo <eduardo@optimmus.io>
2023-07-31 19:23:15 -0300

    feat: sync prices concurrency

--- sync_prices.ts
+++ sync_prices.ts
@@ -1,20 +1,31 @@
 import { MongooseServer } from "./database";
+import { Item } from "./interface";
 import { Warframe } from "./warframe";
 
 async function main() {
     await MongooseServer.startConnectionPromise();
     const m = new Warframe();
     console.time('warframe');
+    let concurrencyLimit = 5;
+    async function processEntry(item) {
+        const market = await m.getWarframeItemOrders(item);
+        await m.saveItem(item.id, { market });
+    }
     m.getItemsDatabase().then(async items => {
-        let idx = 0;
-        for (let item of items) {
-            console.log("Loading", idx, items.length);
-            const market = await m.getWarframeItemOrders(item);
-            await m.saveItem(item.id, { market });
-            idx++;
-        }
-        console.timeEnd('warframe');
-        process.exit(1);
+        const processQueue = items.map(async (entry:Item, index) => {
+            await processEntry(entry);
+            console.log(`Finished processing entry ${index + 1}`);
+        });
+        const processWithConcurrencyLimit = async () => {
+            for (let i = 0; i < items.length; i += concurrencyLimit) {
+                const chunk = processQueue.slice(i, i + concurrencyLimit);
+                await Promise.all(chunk);
+            }
+            console.timeEnd('warframe');
+            process.exit(1);
+        };
+
+        processWithConcurrencyLimit();
     });
 }
 
--- warframe.ts
+++ warframe.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 import axiosRetry from 'axios-retry';
 import { SocksProxyAgent } from "socks-proxy-agent";
+import { sleep } from "./Express/config";
 import { MongooseServer, Schema } from "./database";
 import { Item, OrdersWarframe, StatisticsWarframe, WarframeItemSingle, WarframeItems } from "./interface";
 import privateProxy from "./proxy";
@@ -49,14 +50,20 @@ class Warframe {
         const res = await this.axios.get(url).then(res => res.data);
         return res;
     }
-    async getWarframeItemOrders(item: Item): Promise<{buy: number, sell:number, volume:number}> {
-        const url = `https://api.warframe.market/v1/items/${item.url_name}/orders`;
-        const res = await this.axios.get(url).then(res => res.data);
-        const itemSet = item.items_in_set[0];
-        let max_rank = itemSet.mod_max_rank;
-        console.log("Max Rank", max_rank);
-        const { volume } = await this.getWarframeItemStatistics(item, max_rank);
-        return { ...this.getWarframeItemBuySellPrice(res, max_rank), volume };
+    async getWarframeItemOrders(item: Item): Promise<{ buy: number, sell: number, volume: number }> {
+        try {
+            const url = `https://api.warframe.market/v1/items/${item.url_name}/orders`;
+            const res = await this.axios.get(url).then(res => res.data);
+            const itemSet = item.items_in_set[0];
+            let max_rank = itemSet.mod_max_rank;
+            console.log("Max Rank", max_rank);
+            const { volume } = await this.getWarframeItemStatistics(item, max_rank);
+            return { ...this.getWarframeItemBuySellPrice(res, max_rank), volume };
+        } catch (e) {
+            console.log("Error, try again in 5000ms", e.message);
+            await sleep(5000);
+            return this.getWarframeItemOrders(item);
+        }    
     }
     async getWarframeItemStatistics(item: Item, max_rank: number | undefined): Promise<{volume:number}> {
         const url = `https://api.warframe.market/v1/items/${item.url_name}/statistics`;
