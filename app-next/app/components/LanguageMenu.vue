c50438bb76a987ef4889b709bc6c76f2296869e8
eduair94 <49965850+eduair94@users.noreply.github.com>
2026-07-14 06:14:06 -0300

    feat(migration): port LanguageMenu to Vue3/Vuetify3/i18n v9

---
+++ app-next/app/components/LanguageMenu.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="text-center">
+    <v-menu>
+      <template #activator="{ props }">
+        <v-btn color="primary" v-bind="props">
+          {{ locale }} <v-icon>mdi-chevron-down</v-icon>
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item
+          v-for="(item, index) in availableLocales"
+          :key="index"
+          link
+          :to="switchLocalePath(item.code)"
+        >
+          <v-list-item-title>{{ item.name }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { locale, locales } = useI18n()
+const switchLocalePath = useSwitchLocalePath()
+
+const availableLocales = computed(() =>
+  locales.value.filter((i) => i.code !== locale.value),
+)
+</script>
